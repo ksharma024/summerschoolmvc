071885121efa9c6caf4934b3905667f0fceca1d6
Karn Sharma <karn.sharma024@gmail.com>
2016-07-12 10:04:37 -0400

    Initial Build Up

---
+++ SummerSchoolMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SummerSchoolMVC.Startup))]
+namespace SummerSchoolMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
